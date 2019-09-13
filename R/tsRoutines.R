a06f164f7d2ff41a0d4805a6c22654d190fc99d0
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-09-13 01:09:55 -0400

    New function for data aggregation and visualization

---
+++ R/tsRoutines.R
@@ -0,0 +1,27 @@
+#' Track your Success over Time
+#' 
+#' Aggregates success rate by day, and graphs the results as a time series of sorts if plot is set to TRUE.
+#'
+#' @param directory the directory in which the routines are kept
+#' @param plot whether to plot the results
+#' @param Other various graphical parameters
+#'
+#' @return Returns a data frame with the success rate by day, and plots a graph if plot is set to true.
+#' @export
+#'
+
+tsRoutines <- function(directory = getwd(), plot = T, 
+                       ylim = c(0, 1), cex = 0.1, xlab = "Time", 
+                       ylab = "Success Rate", ...) {
+  results <- routinesRecorded(directory = directory)
+  results[ , 1] <- as.Date(results[ , 1])
+  results <- aggregate(results[ , 3], by = list(Time = results$Time), 
+                       FUN = mean, na.rm = T)
+  colnames(results)[2] <- "Success Rate"
+  if (isTRUE(plot)) {
+    plot(results, cex = cex, xlab = xlab, ylab = ylab, 
+         ylim = ylim, ...) +
+      lines(results)
+  }
+  return(results)
+}
